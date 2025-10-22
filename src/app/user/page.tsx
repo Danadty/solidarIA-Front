7ec1fdfa6b0ec776801896ff6929874edcac72e4
yoel <aquispec@fi.uba.ar>
2025-10-22 18:46:56 -0300

    remove files

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { useEffect } from 'react';
-import { UserAPI } from 'src/lib'; // ajustá la ruta según tu estructura
+import { UserAPI } from 'src/lib';
 
 export default function Page() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const res = await UserAPI.getAll();
-        console.log('✅ Users fetched:', res.data);
+        console.log('Users fetched:', res.data);
       } catch (err) {
-        console.error('❌ Error fetching users:', err);
+        console.error('Error fetching users:', err);
       }
     };
 
