23df5d3cea2f3793d0a781935a3ca1dab4477723
Esteban <estebankaraputny18@gmail.com>
2025-10-27 12:12:23 -0300

    :bug: Fix: function fetchDataWithToken

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,12 +1,11 @@
 // --- Llamado a la api---
-// const fechtData = async (endpoint: string, token: string) => {
+
 const fechtData = async (endpoint: string) => {
   try{
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`, { 
       method: 'GET',
       headers:{
         'Content-Type': 'application/json',
-        // ...(token ? { Authorization: `Bearer ${token}` } : {}),
       },
       cache: "no-store",
     });
@@ -24,4 +23,32 @@ const fechtData = async (endpoint: string) => {
   }
 }
 
-export { fechtData };
+/*Fetch con token */
+
+
+const fechtDataWithToken = async (endpoint: string, token: string) => {
+  try{
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`, { 
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json',
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+      cache: "no-store",
+    });
+
+      if(!response.ok){
+        const errorMessage = await response.json().catch(() => ({}));
+        throw new Error (errorMessage || "Error a obtener datos");
+      };
+
+      const data = await response.json();
+      return data;
+
+  } catch (error: any){
+    throw new Error(error.message || "Error de conexión");
+  }
+}
+
+
+export { fechtData, fechtDataWithToken };
